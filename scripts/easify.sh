62590359d641569a3c84065b779e3b445475a5ce
adrsha <adrsha@github.com>
2023-07-13 18:52:30 +0545

    betterment

---
+++ scripts/easify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hyprctl plugin load /hdd/gitapps/hyprland-plugins/hyprbars/hyprbars.so
+hyprctl plugin load /hdd/gitapps/hyprfocus/hyprfocus.so
---
+++ scripts/uneasify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hyprctl plugin unload /hdd/gitapps/hyprland-plugins/hyprbars/hyprbars.so
+hyprctl plugin unload /hdd/gitapps/hyprfocus/hyprfocus.so
